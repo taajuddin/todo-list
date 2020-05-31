250c0d43cb8ee151958d3ba20748c3406ae11eaa
taajuddin <taajraza@gmail.com>
2020-05-31 14:36:46 +0530

    Updates

---
+++ precache-manifest.21a7fe4526bf98ddad35a3d32966a8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1208b9ae992c4ba8bb29a1351df21ef5",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "a0d7350d47b334d51987",
+    "url": "/todo-list/static/css/main.848f4a73.chunk.css"
+  },
+  {
+    "revision": "69e9f4db48797e1792db",
+    "url": "/todo-list/static/js/2.84e16bad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.84e16bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d7350d47b334d51987",
+    "url": "/todo-list/static/js/main.c2f94d21.chunk.js"
+  },
+  {
+    "revision": "2b909f8b49d22c3e9c72",
+    "url": "/todo-list/static/js/runtime-main.5e1967fb.js"
+  }
+]);
